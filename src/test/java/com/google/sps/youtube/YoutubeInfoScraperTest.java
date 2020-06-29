43973da027693eddda90a8ed6c35714a48982338
Josef Jankowski <jankowski.josef99@gmail.com>
2020-06-29 23:53:10 +0000

    Removed empty comment.

--- src/test/java/com/google/sps/youtube/YoutubeInfoScraperTest.java
+++ src/test/java/com/google/sps/youtube/YoutubeInfoScraperTest.java
@@ -41,7 +41,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-/** */
 @RunWith(MockitoJUnitRunner.class)
 public final class YoutubeInfoScraperTest {
 
