9112e5c05f9b637fa6224f543210fe010869d2d4
Margaret Chan <mchan20822@gmail.com>
2020-06-30 03:28:09 +0000

    Revert "Revert "Make files for all classes""
    This reverts commit f68c59a05589b8c72a5c26eaa939f6a128da4313.

---
+++ src/test/java/com/google/step/youtube/DescriptionParserTest.java
@@ -0,0 +1,11 @@
+package com.google.step.youtube;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+// @RunWith(JUnit4.class)
+public final class DescriptionParserTest {
+
+}
