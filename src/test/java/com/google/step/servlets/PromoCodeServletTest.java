24bf6fc47017667a6d3e0f805b216b690e4428e4
Margaret Chan <mchan20822@gmail.com>
2020-06-30 18:20:39 +0000

    Add test file for PromoCodeServlet

---
+++ src/test/java/com/google/step/servlets/PromoCodeServletTest.java
@@ -0,0 +1,12 @@
+package com.google.step.servlets;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+// @RunWith(JUnit4.class)
+public final class PromoCodeServletTest {
+
+}
+
