a11412d0c39e9335133a0649b363b17f735a8171
Josef Jankowski <jankowski.josef99@gmail.com>
2020-07-06 21:46:26 +0000

    Removed another unused import statement.

--- src/main/java/com/google/step/youtube/YoutubeInfoScraper.java
+++ src/main/java/com/google/step/youtube/YoutubeInfoScraper.java
@@ -2,7 +2,6 @@
 package com.google.step.youtube;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
