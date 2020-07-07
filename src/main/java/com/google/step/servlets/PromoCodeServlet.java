eadc2e46629c0382d5665297ab2cb70abab38db9
Dante Allen <dallen68@gatech.edu>
2020-07-07 16:35:50 +0000

    Reverted rating code

--- src/main/java/com/google/step/servlets/PromoCodeServlet.java
+++ src/main/java/com/google/step/servlets/PromoCodeServlet.java
@@ -1,5 +1,18 @@
 package com.google.step.servlets;
 
-public class PromoCodeServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/promo-code")
+public class PromoCodeServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,9 +1,10 @@
-function ratePromoCode(tableRow, rating) {
-    var videoUrl = document.getElementById('promoCodeTable').rows[tableRow].cells[0].firstChild.getAttribute("href");
-    console.log(videoUrl);
-    fetch('/promo-code?rating=' + rating + '&videoUrl=' + videoUrl, {method: 'POST'});
-    getCodes();
-}
+//This function is not currently in use but may be used in further implementations
+// function ratePromoCode(tableRow, rating) {
+//     var videoUrl = document.getElementById('promoCodeTable').rows[tableRow].cells[0].firstChild.getAttribute("href");
+//     console.log(videoUrl);
+//     fetch('/promo-code?rating=' + rating + '&videoUrl=' + videoUrl, {method: 'POST'});
+//     getCodes();
+// }
 
 function getCodes() {
     fetch('/promo-code').then(response => response.json()).then((codes) => {
@@ -12,23 +13,14 @@ function getCodes() {
                     `<tr id="row0">
                         <th>VideoURL</th>
                         <th>PROMO CODE</th>
-                        <th>Rating</th>
-                        <th><button>Like</button></th>
-                        <th><button>Dislike</button></th>
                     </tr>`;
         const numOfCodes = Object.keys(codes).length;
         for (i = 0; i < numOfCodes; i++) {
             var row = tableEl.insertRow(-1);
             var videoUrl = row.insertCell(0);
             var promoCode = row.insertCell(1);
-            var rating = row.insertCell(2);
-            var likeButton = row.insertCell(3);
-            var dislikeButton = row.insertCell(4);
             videoUrl.innerHTML = '<a href="' + codes[i].videoUrl +'">' + codes[i].videoUrl + '</a>'
             promoCode.innerHTML = codes[i].promoCode;
-            rating.innerHTML = codes[i].rating;
-            likeButton.innerHTML = '<button onClick="ratePromoCode(' + i + ', 1)">Like</button>'
-            dislikeButton.innerHTML = '<button onClick="ratePromoCode(' + i + ', -1)">Dislike</button>'
         }
     });
 }
