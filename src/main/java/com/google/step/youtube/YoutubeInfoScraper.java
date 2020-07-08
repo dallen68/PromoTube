04b22be6dcec6678e20b9144be116653e74a2587
josefj1519 <jankowski.josef99@gmail.com>
2020-07-08 12:54:23 -0700

    Made the response final and removed unused imports.

--- src/main/java/com/google/step/youtube/YoutubeInfoScraper.java
+++ src/main/java/com/google/step/youtube/YoutubeInfoScraper.java
@@ -1,19 +1,15 @@
 
 package com.google.step.youtube;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.ChannelListResponse;
 import com.google.api.services.youtube.YouTubeRequestInitializer;
 import com.google.api.services.youtube.model.Channel;
-import com.google.api.services.youtube.model.ChannelContentDetails;
-import com.google.api.services.youtube.model.ChannelContentDetails.RelatedPlaylists;
 import com.google.api.services.youtube.model.PlaylistItemListResponse;
 import com.google.api.services.youtube.model.PlaylistItem;
 import com.google.api.services.youtube.model.PlaylistItemSnippet;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
@@ -31,71 +27,64 @@ public class YoutubeInfoScraper {
 
   private final YouTube youTubeClient;
 
-  public YoutubeInfoScraper(YouTube youTubeClient) {
+  public YoutubeInfoScraper(final YouTube youTubeClient) {
       this.youTubeClient = youTubeClient;
   }
 
   public YoutubeInfoScraper() {
-    this(new YouTube.Builder(new NetHttpTransport(),
-        JacksonFactory.getDefaultInstance(),
-        /* httpRequestInitializer= */ null)
-        .setApplicationName(APPLICATION_NAME)
-        .setYouTubeRequestInitializer(new YouTubeRequestInitializer(API_KEY))
-        .build());
+      this(new YouTube.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
+              /* httpRequestInitializer= */ null).setApplicationName(APPLICATION_NAME)
+                      .setYouTubeRequestInitializer(new YouTubeRequestInitializer(API_KEY)).build());
   }
 
   /**
-  * @param channelId Id of a channel. Can be found in channel's urls.
-  * e.g. https://www.youtube.com/channel/<CHANNEL-ID>
-  * @return an optional string of the channel's upload playlist id. The optional will be empty 
-  * if id is invalid or no items were found.
-  */
-  public Optional<String> scrapeChannelUploadPlaylist(String channelId)
-    throws IOException {
-        ChannelListResponse response = youTubeClient.channels().list("contentDetails").setId(channelId).execute();
-        // getItems() return null when no items match the criteria (channelId). 
-        if (response.getItems()==null || response.getItems().isEmpty()) {
-            return Optional.empty();
-        }
-        List<Channel> channelsInfo = response.getItems();
-        // Since we are only requesting one channel-id, we only get one item back.  
-        return Optional.of(channelsInfo.get(0).getContentDetails().getRelatedPlaylists().getUploads());
-   }
+   * @param channelId Id of a channel. Can be found in channel's urls. e.g.
+   *                  https://www.youtube.com/channel/<CHANNEL-ID>
+   * @return an optional string of the channel's upload playlist id. The optional
+   *         will be empty if id is invalid or no items were found.
+   */
+  public Optional<String> scrapeChannelUploadPlaylist(final String channelId) throws IOException {
+      final ChannelListResponse response = youTubeClient.channels().list("contentDetails").setId(channelId).execute();
+      // getItems() return null when no items match the criteria (channelId).
+      if (response.getItems() == null || response.getItems().isEmpty()) {
+          return Optional.empty();
+      }
+      final List<Channel> channelsInfo = response.getItems();
+      // Since we are only requesting one channel-id, we only get one item back.
+      return Optional.of(channelsInfo.get(0).getContentDetails().getRelatedPlaylists().getUploads());
+  }
 
   /**
-  * @param uploadId Id of a channel's upload playlist.
-  * @return an optional list of PlaylistItems. Each item contains a video-id, description and a date. 
-  * The optional will be empty if id is invalid or no items were found.
-  */
-   public Optional<List<PlaylistItem>> scrapePlaylistItems(String uploadId)
-    throws IOException {
-       PlaylistItemListResponse response = youTubeClient.playlistItems().list("snippet")
-            .setMaxResults(50L)
-            .setPlaylistId(uploadId)
-            .execute();
-        // getItems() return null when no items match the criteria (uploadId). 
-        if (response.getItems()==null || response.getItems().isEmpty()) {
-            return Optional.empty();
-        }
-        return Optional.of(response.getItems());
-   }
+   * @param uploadId Id of a channel's upload playlist.
+   * @return an optional list of PlaylistItems. Each item contains a video-id,
+   *         description and a date. The optional will be empty if id is invalid
+   *         or no items were found.
+   */
+  public Optional<List<PlaylistItem>> scrapePlaylistItems(final String uploadId) throws IOException {
+      final PlaylistItemListResponse response = youTubeClient.playlistItems().list("snippet").setMaxResults(50L)
+              .setPlaylistId(uploadId).execute();
+      // getItems() return null when no items match the criteria (uploadId).
+      if (response.getItems() == null || response.getItems().isEmpty()) {
+          return Optional.empty();
+      }
+      return Optional.of(response.getItems());
+  }
 
   /**
-  * @param uploadId Id of a channel's upload playlist.
-  * @return an optional list of PromoCode objects.  the optional will return empty
-  * if id is invalid or no items in the playlist are found.  
-  */
-   public Optional<List<PromoCode>> scrapePromoCodesFromPlaylist(String uploadId)
-    throws IOException {
-        Optional<List<PlaylistItem>> playlistItems = scrapePlaylistItems(uploadId);
-        if (playlistItems.isEmpty()) {
-            return Optional.empty();
-        }
-        List<PromoCode> promoCodes = new ArrayList<>(); 
-        for(PlaylistItem item : playlistItems.get()) {
-            PlaylistItemSnippet snippet = item.getSnippet();
-            List<String> itemPromoCodes = DescriptionParser.parse(snippet.getDescription());
-            for(String promocode : itemPromoCodes){
+   * @param uploadId Id of a channel's upload playlist.
+   * @return an optional list of PromoCode objects. the optional will return empty
+   *         if id is invalid or no items in the playlist are found.
+   */
+  public Optional<List<PromoCode>> scrapePromoCodesFromPlaylist(final String uploadId) throws IOException {
+      final Optional<List<PlaylistItem>> playlistItems = scrapePlaylistItems(uploadId);
+      if (playlistItems.isEmpty()) {
+          return Optional.empty();
+      }
+      final List<PromoCode> promoCodes = new ArrayList<>();
+      for (final PlaylistItem item : playlistItems.get()) {
+          final PlaylistItemSnippet snippet = item.getSnippet();
+          final List<String> itemPromoCodes = DescriptionParser.parse(snippet.getDescription());
+          for (final String promocode : itemPromoCodes) {
                 promoCodes.add(PromoCode.create(promocode, snippet.getResourceId().getVideoId(), 
                     new Date(snippet.getPublishedAt().getValue())));
             }
