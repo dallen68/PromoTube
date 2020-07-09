d1533febbd8d34f0e18b1ca592a64622c17dc3bb
josefj1519 <jankowski.josef99@gmail.com>
2020-07-09 11:52:08 -0700

    Alphabetize the static imports.

--- src/test/java/com/google/step/youtube/YoutubeInfoScraperTest.java
+++ src/test/java/com/google/step/youtube/YoutubeInfoScraperTest.java
@@ -14,12 +14,12 @@
 
 package com.google.step.youtube;
 
-import static org.mockito.Mockito.anyString;
-import static org.junit.Assert.assertThrows;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
-import static org.hamcrest.Matchers.equalTo;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.model.Channel;
