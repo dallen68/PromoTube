b924477fb8a9dc96958fdfc191947f1f766f2f90
Dantebot <dallen68@gatech.edu>
2020-07-13 12:38:19 -0400

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -9,16 +9,18 @@ async function displayCodes() {
                     <th>PROMO CODE</th>
                 </tr>`;
     if (codes == 0) {
-        var row = tableEl.insertRow(-1);
+        let row = tableEl.insertRow(-1);
         var noCodes = row.insertCell(0);
         noCodes.innerHTML = '<p>Sorry! There are no codes with this Id.</p>'
     } else {
         const numOfCodes = Object.keys(codes).length;
         for (i = 0; i < numOfCodes; i++) {
-            var row = tableEl.insertRow(-1);
+            let row = tableEl.insertRow(-1);
             var videoUrl = row.insertCell(0);
             var promoCode = row.insertCell(1);
-            videoUrl.innerHTML = '<a href="https://www.youtube.com/watch?v=' + codes[i].videoId +'">https://www.youtube.com/watch?v=' + codes[i].videoId + '</a>';
+            videoUrl.innerHTML = '<a href="https://www.youtube.com/watch?v='
+               + codes[i].videoId +'">https://www.youtube.com/watch?v='
+               + codes[i].videoId + '</a>';
             promoCode.innerHTML = codes[i].promoCode;
         }
     }
