868820a5302c7ace6f11905c0f20c80e4d410fd4
josefj1519 <jankowski.josef99@gmail.com>
2020-07-20 16:26:28 -0700

    Add buttons to chose between business and channel.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,19 +1,25 @@
 async function displayCodes() {
     var formInput = document.getElementById('formInput').value;
-    const response = await fetch('/promo-code?formInput=' + formInput);
+    const selected = document.querySelector('input[name="searchOption"]:checked');
+    var response; 
+    // TODO: Change name of URI's to appropriate servlet names.
+    if(selected.id === "channel"){
+        response = await fetch('/promo-code?formInput=' + formInput);
+    } else {
+       response = await fetch('/promo-code?formInput=' + formInput);
+    }
     const codes = await response.json();
-
-    
+    setTable(codes);
 }
 
-function setTable() {
+function setTable(codes) {
     const tableEl = document.getElementById('promoCodeTable');
     tableEl.innerHTML =
                 `<tr>
                     <th>VideoURL</th>
                     <th>PROMO CODE</th>
                 </tr>`;
-    if (codes == 0) {
+    if (codes === 0) {
         let row = tableEl.insertRow(-1);
         var noCodes = row.insertCell(0);
         noCodes.innerHTML = '<p>Sorry! There are no codes with this Id.</p>'
@@ -24,8 +30,8 @@ function setTable() {
             var videoUrl = row.insertCell(0);
             var promoCode = row.insertCell(1);
             videoUrl.innerHTML = '<a href="https://www.youtube.com/watch?v='
-               + codes[i].videoId +'" target="_blank">'
-               + codes[i].videoTitle + '</a>';
+                + codes[i].videoId + '" target="_blank">'
+                + codes[i].videoTitle + '</a>';
             promoCode.innerHTML = codes[i].promoCode;
         }
     }
