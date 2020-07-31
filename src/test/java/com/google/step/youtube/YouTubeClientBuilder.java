c4eff5b25875facae3dd41599fdaa8c4aae0a0ea
josefj1519 <jankowski.josef99@gmail.com>
2020-07-31 10:36:16 -0700

    Add utility class to help integrate tests.

---
+++ src/test/java/com/google/step/youtube/YouTubeClientBuilder.java
@@ -0,0 +1,148 @@
+package com.google.step.youtube;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.api.client.util.DateTime;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTube.Channels;
+import com.google.api.services.youtube.YouTube.PlaylistItems;
+import com.google.api.services.youtube.YouTube.Search;
+import com.google.api.services.youtube.YouTube.Videos;
+import com.google.api.services.youtube.model.Channel;
+import com.google.api.services.youtube.model.ChannelContentDetails;
+import com.google.api.services.youtube.model.ChannelContentDetails.RelatedPlaylists;
+import com.google.api.services.youtube.model.ChannelListResponse;
+import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
+import com.google.api.services.youtube.model.PlaylistItemSnippet;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoListResponse;
+import com.google.api.services.youtube.model.VideoSnippet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+
+public class YouTubeClientBuilder {
+
+    private static Channels.List mockListChannels;
+    private static Videos.List mockListVideos;
+    private static Search.List mockListSearch;
+    private static PlaylistItems.List mockListPlaylistItems;
+
+    private static final String UPLOAD_ID = "UPLOAD_ID";
+    private static final String VIDEO_ID = "VIDEO_ID";
+    private static final String VIDEO_TITLE = "VIDEO_TITLE";
+    private static final Date DATE = new Date(0L);
+
+    public static final String KEYWORD = "KEYWORD";
+    public static final String PROMOCODE = "PROMOCODE";
+    public static final String PROMOCODE_DESCRIPTION = "Get 20% off your first monthly box and enter the code "
+            + PROMOCODE;
+    public static final String DESCRIPTION = "DESCRIPTION WITH NO PROMOCODE";
+    public static final String KEYWORD_DESCRIPTION = "Get 25% off your first purchase at " + KEYWORD + " with the code "
+            + PROMOCODE;
+
+    public static YouTube create() throws IOException {
+        YouTube mockYouTubeClient = mock(YouTube.class);
+
+        Channels mockChannels = mock(YouTube.Channels.class);
+        mockListChannels = mock(YouTube.Channels.List.class);
+        when(mockYouTubeClient.channels()).thenReturn(mockChannels);
+        when(mockChannels.list("contentDetails")).thenReturn(mockListChannels);
+        when(mockListChannels.setId(anyString())).thenReturn(mockListChannels);
+        when(mockListChannels.setForUsername(anyString())).thenReturn(mockListChannels);
+
+        PlaylistItems mockPlaylistItems = mock(YouTube.PlaylistItems.class);
+        mockListPlaylistItems = mock(YouTube.PlaylistItems.List.class);
+        when(mockYouTubeClient.playlistItems()).thenReturn(mockPlaylistItems);
+        when(mockPlaylistItems.list("snippet")).thenReturn(mockListPlaylistItems);
+        when(mockListPlaylistItems.setMaxResults(YouTubeInfoScraper.MAX_PLAYLIST_RESULTS))
+                .thenReturn(mockListPlaylistItems);
+        when(mockListPlaylistItems.setPlaylistId(anyString())).thenReturn(mockListPlaylistItems);
+
+        Videos mockVideos = mock(YouTube.Videos.class);
+        mockListVideos = mock(Videos.List.class);
+        when(mockYouTubeClient.videos()).thenReturn(mockVideos);
+        when(mockVideos.list("snippet")).thenReturn(mockListVideos);
+        when(mockListVideos.setId(anyString())).thenReturn(mockListVideos);
+        when(mockListVideos.setFields("items(id, snippet(publishedAt, title, description))"))
+                .thenReturn(mockListVideos);
+
+        Search mockSearch = mock(Search.class);
+        mockListSearch = mock(Search.List.class);
+        when(mockYouTubeClient.search()).thenReturn(mockSearch);
+        when(mockSearch.list("snippet")).thenReturn(mockListSearch);
+        when(mockListSearch.setMaxResults(YouTubeInfoScraper.MAX_SEARCH_RESULTS)).thenReturn(mockListSearch);
+        when(mockListSearch.setQ(anyString())).thenReturn(mockListSearch);
+
+        return mockYouTubeClient;
+    }
+
+    public Channels.List getMockListChannels() {
+        return mockListChannels;
+    }
+
+    public PlaylistItems.List getMockListPlaylistItems() {
+        return mockListPlaylistItems;
+    }
+
+    public Videos.List getMockListVideos() {
+        return mockListVideos;
+    }
+
+    public Search.List getMockListSearch() {
+        return mockListSearch;
+    }
+
+    public static PromoCode newPromoCode() {
+        return PromoCode.create(PROMOCODE, PROMOCODE_DESCRIPTION, VIDEO_ID, VIDEO_TITLE, DATE);
+    }
+
+    public ChannelListResponse newBasicChannelResponse() {
+        ChannelListResponse testChannelResponse = new ChannelListResponse();
+        Channel channel = new Channel();
+        channel.setContentDetails(
+                new ChannelContentDetails().setRelatedPlaylists(new RelatedPlaylists().setUploads(UPLOAD_ID)));
+        testChannelResponse.setItems(Arrays.asList(channel));
+        return testChannelResponse;
+    }
+
+    public PlaylistItemListResponse newBasicPlaylistResponse() {
+        PlaylistItemListResponse testPlaylistResponse = new PlaylistItemListResponse();
+        testPlaylistResponse.setItems(Arrays.asList(newBasicPlaylistItem(PROMOCODE_DESCRIPTION)));
+        return testPlaylistResponse;
+    }
+
+    public PlaylistItem newBasicPlaylistItem(String description) {
+        return new PlaylistItem()
+                .setSnippet(new PlaylistItemSnippet().setDescription(description).setPublishedAt(new DateTime(DATE))
+                        .setTitle(VIDEO_TITLE).setResourceId(new ResourceId().setVideoId(VIDEO_ID)));
+    }
+
+    public VideoListResponse newBasicVideoListResponse() {
+        VideoListResponse testVideoResponse = new VideoListResponse();
+        testVideoResponse.setItems(Arrays.asList(new Video(), new Video()));
+        return testVideoResponse;
+    }
+
+    public Video newBasicVideoResponse(String description) {
+        return new Video().setId(VIDEO_ID).setSnippet(new VideoSnippet().setTitle(VIDEO_TITLE)
+                .setDescription(description).setPublishedAt(new DateTime(DATE)));
+    }
+
+    public SearchListResponse newBaSearchListResponse() {
+        SearchListResponse testSearchResponse = new SearchListResponse();
+        testSearchResponse.setItems(Arrays.asList(newBasicSearchResult(), newBasicSearchResult()));
+        return testSearchResponse;
+    }
+
+    public SearchResult newBasicSearchResult() {
+        return new SearchResult().setId(new ResourceId().setVideoId(VIDEO_ID));
+    }
+}
--- src/test/java/com/google/step/youtube/YouTubeInfoScraperTest.java
+++ src/test/java/com/google/step/youtube/YouTubeInfoScraperTest.java
@@ -14,32 +14,25 @@
 
 package com.google.step.youtube;
 
+import static com.google.step.youtube.YouTubeClientBuilder.PROMOCODE_DESCRIPTION;
+import static com.google.step.youtube.YouTubeClientBuilder.KEYWORD_DESCRIPTION;
+import static com.google.step.youtube.YouTubeClientBuilder.DESCRIPTION;
+import static com.google.step.youtube.YouTubeClientBuilder.PROMOCODE;
+import static com.google.step.youtube.YouTubeClientBuilder.KEYWORD;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
 
-import com.google.api.client.util.DateTime;
-import com.google.api.services.youtube.model.Channel;
 import com.google.api.services.youtube.model.ChannelListResponse;
-import com.google.api.services.youtube.model.ChannelContentDetails;
-import com.google.api.services.youtube.model.ChannelContentDetails.RelatedPlaylists;
 import com.google.api.services.youtube.model.PlaylistItem;
-import com.google.api.services.youtube.model.PlaylistItemSnippet;
 import com.google.api.services.youtube.model.PlaylistItemListResponse;
-import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
-import com.google.api.services.youtube.model.VideoSnippet;
 import com.google.api.services.youtube.YouTube.Channels;
 import com.google.api.services.youtube.YouTube.PlaylistItems;
 import com.google.api.services.youtube.YouTube.Videos;
 import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.YouTube.Search;
-import com.google.api.services.youtube.YouTube;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
@@ -53,6 +46,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public final class YouTubeInfoScraperTest {
 
+    private YouTubeClientBuilder ytClientBuilder;
     private YouTubeInfoScraper scraper;
     private Channels.List mockListChannels;
     private Videos.List mockListVideos;
@@ -68,7 +62,6 @@ public final class YouTubeInfoScraperTest {
     private static final String UPLOAD_ID = "UPLOAD_ID";
     private static final String VIDEO_ID = "VIDEO_ID";
     private static final String VIDEO_TITLE = "VIDEO_TITLE";
-    private static final String KEYWORD = "KEYWORD";
     private static final String NO_RESULTS_KEYWORD = "NO_RESULTS_KEYWORD";
     private static final List<String> EMPTY_VIDEO_ID_LIST = Arrays.asList();
     private static final List<String> VIDEO_ID_LIST = Arrays.asList();
@@ -76,39 +69,12 @@ public final class YouTubeInfoScraperTest {
 
     @Before
     public void setUp() throws IOException {
-        YouTube mockYouTubeClient = mock(YouTube.class);
-
-        Channels mockChannels = mock(YouTube.Channels.class);
-        mockListChannels = mock(YouTube.Channels.List.class);
-        when(mockYouTubeClient.channels()).thenReturn(mockChannels);
-        when(mockChannels.list("contentDetails")).thenReturn(mockListChannels);
-        when(mockListChannels.setId(anyString())).thenReturn(mockListChannels);
-        when(mockListChannels.setForUsername(anyString())).thenReturn(mockListChannels);
-
-        PlaylistItems mockPlaylistItems = mock(YouTube.PlaylistItems.class);
-        mockListPlaylistItems = mock(YouTube.PlaylistItems.List.class);
-        when(mockYouTubeClient.playlistItems()).thenReturn(mockPlaylistItems);
-        when(mockPlaylistItems.list("snippet")).thenReturn(mockListPlaylistItems);
-        when(mockListPlaylistItems.setMaxResults(YouTubeInfoScraper.MAX_PLAYLIST_RESULTS))
-                        .thenReturn(mockListPlaylistItems);
-        when(mockListPlaylistItems.setPlaylistId(anyString())).thenReturn(mockListPlaylistItems);
-
-        Videos mockVideos = mock(YouTube.Videos.class);
-        mockListVideos = mock(Videos.List.class);
-        when(mockYouTubeClient.videos()).thenReturn(mockVideos);
-        when(mockVideos.list("snippet")).thenReturn(mockListVideos);
-        when(mockListVideos.setId(anyString())).thenReturn(mockListVideos);
-        when(mockListVideos.setFields("items(id, snippet(publishedAt, title, description))"))
-                .thenReturn(mockListVideos);
-
-        Search mockSearch = mock(Search.class);
-        mockListSearch = mock(Search.List.class);
-        when(mockYouTubeClient.search()).thenReturn(mockSearch);
-        when(mockSearch.list("snippet")).thenReturn(mockListSearch);
-        when(mockListSearch.setMaxResults(YouTubeInfoScraper.MAX_SEARCH_RESULTS)).thenReturn(mockListSearch);
-        when(mockListSearch.setQ(anyString())).thenReturn(mockListSearch);
-    
-        scraper = new YouTubeInfoScraper(mockYouTubeClient);
+        ytClientBuilder = new YouTubeClientBuilder();
+        scraper = new YouTubeInfoScraper(YouTubeClientBuilder.create());
+        mockListChannels = ytClientBuilder.getMockListChannels();
+        mockListPlaylistItems = ytClientBuilder.getMockListPlaylistItems();
+        mockListVideos = ytClientBuilder.getMockListVideos();
+        mockListSearch = ytClientBuilder.getMockListSearch();
     }
 
     @Test
@@ -121,12 +87,7 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapeChannelUploadPlaylist_channelExists() throws IOException {
-        ChannelListResponse testChannelResponse = new ChannelListResponse();
-        Channel channel = new Channel();
-        channel.setContentDetails(
-                new ChannelContentDetails().setRelatedPlaylists(new RelatedPlaylists().setUploads(UPLOAD_ID)));
-        testChannelResponse.setItems(Arrays.asList(channel));
-        when(mockListChannels.execute()).thenReturn(testChannelResponse);
+        when(mockListChannels.execute()).thenReturn(ytClientBuilder.newBasicChannelResponse());
         Optional<String> actual = scraper.scrapeChannelUploadPlaylist(CHANNEL_ID);
         assertThat(actual.get(), equalTo(UPLOAD_ID));
     }
@@ -141,12 +102,7 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapeUserUploadPlaylist_userExists() throws IOException {
-        ChannelListResponse testChannelResponse = new ChannelListResponse();
-        Channel channel = new Channel();
-        channel.setContentDetails(
-                new ChannelContentDetails().setRelatedPlaylists(new RelatedPlaylists().setUploads(UPLOAD_ID)));
-        testChannelResponse.setItems(Arrays.asList(channel));
-        when(mockListChannels.execute()).thenReturn(testChannelResponse);
+        when(mockListChannels.execute()).thenReturn(ytClientBuilder.newBasicChannelResponse());
         Optional<String> actual = scraper.scrapeUserUploadPlaylist(USERNAME);
         assertThat(actual.get(), equalTo(UPLOAD_ID));
     }
@@ -161,8 +117,7 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapePlaylistItems_uploadIdExists() throws IOException {
-        PlaylistItemListResponse testPlaylistResponse = new PlaylistItemListResponse();
-        testPlaylistResponse.setItems(Arrays.asList(new PlaylistItem(), new PlaylistItem()));
+        PlaylistItemListResponse testPlaylistResponse = ytClientBuilder.newBasicPlaylistResponse();
         when(mockListPlaylistItems.execute()).thenReturn(testPlaylistResponse);
         Optional<List<PlaylistItem>> actual = scraper.scrapePlaylistItems(UPLOAD_ID);
         assertThat(actual.get(), equalTo(testPlaylistResponse.getItems()));
@@ -178,17 +133,12 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapePromoCodesFromPlaylist_oneItem() throws IOException {
-        PlaylistItemListResponse testPlaylistResponse = new PlaylistItemListResponse();
-        String description = "Get 20% off your first monthly box and enter the code RO at checkout!";
-        testPlaylistResponse.setItems(Arrays.asList(new PlaylistItem()
-                .setSnippet(new PlaylistItemSnippet().setDescription(description).setPublishedAt(new DateTime(DATE))
-                        .setTitle(VIDEO_TITLE).setResourceId(new ResourceId().setVideoId(VIDEO_ID)))));
+        PlaylistItemListResponse testPlaylistResponse = ytClientBuilder.newBasicPlaylistResponse();
         when(mockListPlaylistItems.execute()).thenReturn(testPlaylistResponse);
         Optional<List<PromoCode>> actual = scraper.scrapePromoCodesFromPlaylist(UPLOAD_ID);
         assertThat(actual.get(),
-                equalTo(Arrays.asList(
-                        PromoCode.create("RO", "Get 20% off your first monthly box and enter the code RO at checkout!",
-                                VIDEO_ID, VIDEO_TITLE, DATE))));
+                equalTo(Arrays.asList( PromoCode.create(PROMOCODE, PROMOCODE_DESCRIPTION,
+                        VIDEO_ID, VIDEO_TITLE, DATE))));
     }
 
     @Test
@@ -203,38 +153,18 @@ public final class YouTubeInfoScraperTest {
     @Test
     public void scrapePromoCodesFromPlaylist_multipleItemsSomeCodesFound() throws IOException {
         PlaylistItemListResponse testPlaylistResponse = new PlaylistItemListResponse();
-        String description1 = "Get 20% off your first monthly box when you sign up at http://boxofawesome.com";
-        String description2 = "Use code LINUS and get 25% off GlassWire";
-        String descriptionWithNoCode = "Check out our UPDATED version which has all the NEW ELEMENTS here: "
-                + "\n https://youtu.be/rz4Dd1I_fX0  The TEETH Song (Memorize Every Tooth): https://youtu.be/PI3hne8C8rU"
-                + "\nDownload on ITUNES: http://bit.ly/12AeW99 ";
-        testPlaylistResponse.setItems(Arrays.asList(
-                new PlaylistItem().setSnippet(
-                        new PlaylistItemSnippet().setDescription(description1).setPublishedAt(new DateTime(DATE))
-                                .setTitle(VIDEO_TITLE).setResourceId(new ResourceId().setVideoId(VIDEO_ID))),
-                new PlaylistItem().setSnippet(
-                        new PlaylistItemSnippet().setDescription(description2).setPublishedAt(new DateTime(DATE))
-                                .setTitle(VIDEO_TITLE).setResourceId(new ResourceId().setVideoId(VIDEO_ID))),
-                new PlaylistItem().setSnippet(new PlaylistItemSnippet().setDescription(descriptionWithNoCode)
-                        .setPublishedAt(new DateTime(DATE)).setTitle(VIDEO_TITLE)
-                        .setResourceId(new ResourceId().setVideoId(VIDEO_ID)))));
+        testPlaylistResponse.setItems(Arrays.asList(ytClientBuilder.newBasicPlaylistItem(PROMOCODE_DESCRIPTION),
+        ytClientBuilder.newBasicPlaylistItem(PROMOCODE_DESCRIPTION), ytClientBuilder.newBasicPlaylistItem(DESCRIPTION)));
         when(mockListPlaylistItems.execute()).thenReturn(testPlaylistResponse);
         Optional<List<PromoCode>> actual = scraper.scrapePromoCodesFromPlaylist(UPLOAD_ID);
-        assertThat(actual.get(), equalTo(Arrays.asList(
-                PromoCode.create("http://boxofawesome.com",
-                        "Get 20% off your first monthly box when you sign up at http://boxofawesome.com", VIDEO_ID, VIDEO_TITLE, DATE),
-                PromoCode.create("LINUS", "Use code LINUS and get 25% off GlassWire", VIDEO_ID, VIDEO_TITLE, DATE))));
+        assertThat(actual.get(), equalTo(Arrays.asList(YouTubeClientBuilder.newPromoCode(), 
+                YouTubeClientBuilder.newPromoCode())));
     }
 
     @Test
     public void scrapePromoCodesFromPlaylist_oneItemNoCodeFound() throws IOException {
         PlaylistItemListResponse testPlaylistResponse = new PlaylistItemListResponse();
-        String descriptionWithNoCode = "Check out our UPDATED version which has all the NEW ELEMENTS here: "
-                + "\n https://youtu.be/rz4Dd1I_fX0  The TEETH Song (Memorize Every Tooth): https://youtu.be/PI3hne8C8rU"
-                + "\nDownload on ITUNES: http://bit.ly/12AeW99 ";
-        testPlaylistResponse.setItems(Arrays.asList(new PlaylistItem().setSnippet(
-                new PlaylistItemSnippet().setDescription(descriptionWithNoCode).setPublishedAt(new DateTime(DATE))
-                        .setTitle(VIDEO_TITLE).setResourceId(new ResourceId().setVideoId(VIDEO_ID)))));
+        testPlaylistResponse.setItems(Arrays.asList(ytClientBuilder.newBasicPlaylistItem(DESCRIPTION)));
         when(mockListPlaylistItems.execute()).thenReturn(testPlaylistResponse);
         Optional<List<PromoCode>> actual = scraper.scrapePromoCodesFromPlaylist(UPLOAD_ID);
         assertThat(actual.get().isEmpty(), equalTo(true));
@@ -250,8 +180,7 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapeVideoInformation_validRequest() throws IOException {
-        VideoListResponse testVideoResponse = new VideoListResponse();
-        testVideoResponse.setItems(Arrays.asList(new Video(), new Video()));
+        VideoListResponse testVideoResponse = ytClientBuilder.newBasicVideoListResponse();
         when(mockListVideos.execute()).thenReturn(testVideoResponse);
         Optional<List<Video>> actual = scraper.scrapeVideoInformation(Arrays.asList(VIDEO_ID, VIDEO_ID));
         assertThat(actual.get(), equalTo(Arrays.asList(new Video(), new Video())));
@@ -267,9 +196,7 @@ public final class YouTubeInfoScraperTest {
 
     @Test
     public void scrapeVideoIdsFromSearch_returnVideoIds() throws IOException {
-        SearchListResponse testSearchResponse = new SearchListResponse();
-        testSearchResponse.setItems(Arrays.asList(new SearchResult().setId(new ResourceId().setVideoId(VIDEO_ID)),
-                new SearchResult().setId(new ResourceId().setVideoId(VIDEO_ID))));
+        SearchListResponse testSearchResponse = ytClientBuilder.newBaSearchListResponse();
         when(mockListSearch.execute()).thenReturn(testSearchResponse);
         Optional<List<String>> actual = scraper.scrapeVideoIdsFromSearch(KEYWORD);
         assertThat(actual.get(), equalTo(Arrays.asList(VIDEO_ID, VIDEO_ID)));
@@ -287,38 +214,22 @@ public final class YouTubeInfoScraperTest {
     @Test
     public void scrapePromoCodesFromVideos_multipleItemsSomeCodesFound() throws IOException {
         VideoListResponse testVideoResponse = new VideoListResponse();
-        // Two company names one promo-code found.
-        String snippet = "DISCOUNT!!! For a limited time only, take 25% off your first purchase at " + KEYWORD
-                + " with the code OVDOINGTHINGS25. This offer is valid online only. ";
-        String description1 = snippet + "\nMust apply code at " + KEYWORD + " checkout. Expires August 31, 2020.";
-        // One keyword two promo-codes found.
-        String description2Truncated = "you want to order food through " + KEYWORD + " go to "
-                        + "https://pmfleet.app.link/zyoaw9s3R6 and use my code A1JZN";
-        String description2 = "And if " + description2Truncated;
-        String descriptionWithNoCode = KEYWORD + " is a great company!";
         testVideoResponse.setItems(Arrays.asList(
-                new Video().setId(VIDEO_ID)
-                        .setSnippet(new VideoSnippet().setTitle(VIDEO_TITLE).setDescription(description1)
-                                .setPublishedAt(new DateTime(DATE))),
-                new Video().setId(VIDEO_ID)
-                        .setSnippet(new VideoSnippet().setTitle(VIDEO_TITLE).setDescription(description2)
-                                .setPublishedAt(new DateTime(DATE))),
-                new Video().setId(VIDEO_ID).setSnippet(new VideoSnippet().setTitle(VIDEO_TITLE)
-                        .setDescription(descriptionWithNoCode).setPublishedAt(new DateTime(DATE)))));
+            ytClientBuilder.newBasicVideoResponse(KEYWORD_DESCRIPTION),
+            ytClientBuilder.newBasicVideoResponse(KEYWORD_DESCRIPTION),
+            ytClientBuilder.newBasicVideoResponse(DESCRIPTION)));
         when(mockListVideos.execute()).thenReturn(testVideoResponse);
         Optional<List<PromoCode>> actual = scraper.scrapePromoCodesFromVideos(KEYWORD, VIDEO_ID_LIST);
         assertThat(actual.get(), equalTo(Arrays.asList(
-                PromoCode.create("OVDOINGTHINGS25", snippet, VIDEO_ID, VIDEO_TITLE, DATE),
-                PromoCode.create("A1JZN", "... " + description2Truncated, VIDEO_ID, VIDEO_TITLE, DATE),
-                PromoCode.create("https://pmfleet.app.link/zyoaw9s3R6", description2, VIDEO_ID, VIDEO_TITLE, DATE))));
+                PromoCode.create(PROMOCODE, KEYWORD_DESCRIPTION, VIDEO_ID, VIDEO_TITLE, DATE),
+                PromoCode.create(PROMOCODE, KEYWORD_DESCRIPTION, VIDEO_ID, VIDEO_TITLE, DATE))));
     }
 
     @Test
     public void scrapePromoCodesFromVideos_oneItemNoCodesFound() throws IOException {
         VideoListResponse testVideoResponse = new VideoListResponse();
         String descriptionWithNoCode = KEYWORD + " is a great company!";
-        testVideoResponse.setItems(Arrays.asList(new Video().setId(VIDEO_ID).setSnippet(new VideoSnippet()
-                .setTitle(VIDEO_TITLE).setDescription(descriptionWithNoCode).setPublishedAt(new DateTime(DATE)))));
+        testVideoResponse.setItems(Arrays.asList(ytClientBuilder.newBasicVideoResponse(descriptionWithNoCode)));
         when(mockListVideos.execute()).thenReturn(testVideoResponse);
         Optional<List<PromoCode>> actual = scraper.scrapePromoCodesFromVideos(KEYWORD, VIDEO_ID_LIST);
         assertThat(actual.get().isEmpty(), equalTo(true));
